90a916c14ad977056047759f977776e0663339e1
Peter Hillerström <peter@hstroem.se>
2017-01-10 21:34:43 +0100

    ProjectionMatrices: Changed to new matrix constructor style.

--- src/sfz/math/ProjectionMatrices.cpp
+++ src/sfz/math/ProjectionMatrices.cpp
@@ -31,12 +31,10 @@ mat4 viewMatrixGL(const vec3& origin, const vec3& dir, const vec3& up) noexcept
 	vec3 xAxis = normalize(cross(up, zAxis)); // To the right
 	vec3 yAxis = cross(zAxis, xAxis); // Up
 
-	return mat4{
-		{xAxis.x, xAxis.y, xAxis.z, -dot(xAxis, origin)},
-		{yAxis.x, yAxis.y, yAxis.z, -dot(yAxis, origin)},
-		{zAxis.x, zAxis.y, zAxis.z, -dot(zAxis, origin)},
-		{0.0f,    0.0f,    0.0f,    1.0f}
-	};
+	return mat4(xAxis.x,  xAxis.y,  xAxis.z,  -dot(xAxis, origin),
+	            yAxis.x,  yAxis.y,  yAxis.z,  -dot(yAxis, origin),
+	            zAxis.x,  zAxis.y,  zAxis.z,  -dot(zAxis, origin),
+	            0.0f,     0.0f,     0.0f,     1.0f);
 }
 
 // Projection matrices (Standard OpenGL [-1, 1] right-handed clip space, GL view space)
@@ -44,12 +42,12 @@ mat4 viewMatrixGL(const vec3& origin, const vec3& dir, const vec3& up) noexcept
 
 mat4 orthogonalProjectionGL(float l, float b, float r, float t, float n, float f) noexcept
 {
-	return mat4{
-		{2.0f / (r - l),  0.0f,            0.0f,             -((r + l) / (r - l))},
-		{0.0f,            2.0f / (t - b),  0.0f,             -((t + b) / (t - b))},
-		{0.0f,            0.0f,            -2.0f / (f - n),  -(f + n) / (f - n)},
-		{0.0f,            0.0f,            0.0f,             1.0f}
-	};
+	return mat4(
+		2.0f / (r - l),  0.0f,            0.0f,             -((r + l) / (r - l)),
+		0.0f,            2.0f / (t - b),  0.0f,             -((t + b) / (t - b)),
+		0.0f,            0.0f,            -2.0f / (f - n),  -(f + n) / (f - n),
+		0.0f,            0.0f,            0.0f,             1.0f
+	);
 }
 
 mat4 orthogonalProjectionGL(vec3 leftBottomNear, vec3 rightTopFar) noexcept
@@ -62,21 +60,21 @@ mat3 orthogonalProjection2DGL(vec2 center, vec2 dimensions) noexcept
 {
 	float a = 2.0f / dimensions.x;
 	float b = 2.0f / dimensions.y;
-	return mat3{
-		{a,     0.0f,  -(center.x * a)},
-		{0.0f,  b,     -(center.y * b)},
-		{0.0f,  0.0f,  1.0f}
-	};
+	return mat3(
+		a,     0.0f,  -(center.x * a),
+		0.0f,  b,     -(center.y * b),
+		0.0f,  0.0f,  1.0f
+	);
 }
 
 mat4 perspectiveProjectionGL(float l, float b, float r, float t, float n, float f) noexcept
 {
-	return mat4{
-		{2.0f * n / (r - l),  0.0f,                (r + l) / (r - l),   0.0f},
-		{0.0f,                2.0f * n / (t - b),  (t + b) / (t - b),   0.0f},
-		{0.0f,                0.0f,                -(f + n) / (f - n),  -2.0f * f * n / (f - n)},
-		{0.0f,                0.0f,                -1.0f,               0.0f}
-	};
+	return mat4(
+		2.0f * n / (r - l),  0.0f,                (r + l) / (r - l),   0.0f,
+		0.0f,                2.0f * n / (t - b),  (t + b) / (t - b),   0.0f,
+		0.0f,                0.0f,                -(f + n) / (f - n),  -2.0f * f * n / (f - n),
+		0.0f,                0.0f,                -1.0f,               0.0f
+	);
 }
 
 mat4 perspectiveProjectionGL(float yFovDeg, float aspectRatio, float zNear, float zFar) noexcept
@@ -91,12 +89,12 @@ mat4 perspectiveProjectionGL(float yFovDeg, float aspectRatio, float zNear, floa
 
 mat4 perspectiveProjectionVkD3d(float l, float b, float r, float t, float n, float f) noexcept
 {
-	return mat4{
-		{(2.0f * n) / (r - l),  0.0f,                  (l + r) / (r - l),  0.0f},
-		{0.0f,                  (2.0f * n) / (t - b),  (t + b) / (t - b),  0.0f},
-		{0.0f,                  0.0f,                  f / (n - f),         n * f / (n - f)},
-		{0.0f,                  0.0f,                  -1.0f,               0.0f}
-	};
+	return mat4(
+		(2.0f * n) / (r - l),  0.0f,                  (l + r) / (r - l),  0.0f,
+		0.0f,                  (2.0f * n) / (t - b),  (t + b) / (t - b),  0.0f,
+		0.0f,                  0.0f,                  f / (n - f),         n * f / (n - f),
+		0.0f,                  0.0f,                  -1.0f,               0.0f
+	);
 }
 
 mat4 perspectiveProjectionVkD3d(float yFovDeg, float aspectRatio, float zNear, float zFar) noexcept
@@ -138,12 +136,12 @@ mat4 reverseInfinitePerspectiveProjectionVkD3d(float l, float b, float r, float 
 	// In order to flip the screen vertically and make the result correct on OpenGL with
 	// glClipControl(GL_UPPER_LEFT, GL_ZERO_TO_ONE), also needs to change front face to clockwise,
 	// i.e. glFrontFace(GL_CW)
-	return mat4{
-		{(2.0f * n) / (r - l),  0.0f,                   (l + r) / (r - l),    0.0f},
-		{0.0f,                  (-2.0f * n) / (t - b),   -(t + b) / (t - b),  0.0f},
-		{0.0f,                  0.0f,                   0.0f,                 n},
-		{0.0f,                  0.0f,                   -1.0f,                -n}
-	};
+	return mat4(
+		(2.0f * n) / (r - l),  0.0f,                   (l + r) / (r - l),    0.0f,
+		0.0f,                  (-2.0f * n) / (t - b),   -(t + b) / (t - b),  0.0f,
+		0.0f,                  0.0f,                   0.0f,                 n,
+		0.0f,                  0.0f,                   -1.0f,                -n
+	);
 }
 
 mat4 reverseInfinitePerspectiveProjectionVkD3d(float yFovDeg, float aspect, float zNear) noexcept
