d8f6901dd95e3a3254a14cc53e6820a5293874d1
Peter Hillerström <peter@hstroem.se>
2018-10-27 14:38:48 +0200

    Remove deprecated get admin functions from windows build scripts

--- gen_scripts/gen_visualstudio2017.bat
+++ gen_scripts/gen_visualstudio2017.bat
@@ -1,30 +1,3 @@
-@echo off
-
-: Admin escalation script from: https://sites.google.com/site/eneerge/scripts/batchgotadmin
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-
 : Set working directory to location of this file
 cd %dp0
 
@@ -38,5 +11,3 @@ cd build_msvc2017
 
 : Generate Visual Studio solution
 cmake .. -G "Visual Studio 15 2017 Win64" -T v140
-
-PAUSE
