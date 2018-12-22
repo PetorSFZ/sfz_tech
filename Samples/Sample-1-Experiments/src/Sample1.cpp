e0712b3313caee598b79fc0e28fd844dc27e2e7b
Peter Hillerström <peter.hillerstrom93@gmail.com>
2018-12-22 13:39:57 +0100

    Started adding infrastructure for samples

---
+++ Samples/Sample-1-Experiments/src/Sample1.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", "Hello World!");
+	return 0;
+}
---
+++ Samples/gen_scripts/gen_visualstudio2017.bat
@@ -0,0 +1,34 @@
+: Run script in its own context
+setlocal
+
+: Set working directory to location of this file
+pushd %~dp0
+
+: Set working directory to root of project
+cd ..
+
+: Create build_msvc2017 directory (and delete old one if it exists)
+rmdir /S /Q build_msvc2017
+mkdir build_msvc2017
+cd build_msvc2017
+
+: Generate Visual Studio solution
+cmake .. -G "Visual Studio 15 2017 Win64" -T host=x64
+
+: Create resources symlinks
+mklink /D res\ ..\res
+
+mkdir Debug
+cd Debug
+mklink /D res\ ..\..\res
+cd ..
+
+mkdir RelWithDebInfo
+cd RelWithDebInfo
+mklink /D res\ ..\..\res
+cd ..
+
+mkdir Release
+cd Release
+mklink /D res\ ..\..\res
+cd ..
