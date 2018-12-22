93b28ea35e6fa02a2c4107717cb046839bffcc27
Peter Hillerström <peter.hillerstrom93@gmail.com>
2018-12-22 16:18:39 +0100

    Sample1: Just create an SDL2 window

--- Samples/Sample-1-Experiments/src/Sample1.cpp
+++ Samples/Sample-1-Experiments/src/Sample1.cpp
@@ -16,16 +16,78 @@
 //    misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+#include <cstdlib>
 #include <cstdio>
 
 #include <SDL.h>
 #include <ZeroG/ZeroG.hpp>
+
+#ifdef _WIN32
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
 
 int main(int argc, char* argv[])
 {
 	(void)argc;
 	(void)argv;
 
-	printf("%s\n", "Hello World!");
-	return 0;
+	// Enable hi-dpi awareness on Windows
+#ifdef _WIN32
+	SetProcessDPIAware();
+#endif
+
+	// Init SDL2
+	if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
+		printf("SDL_Init() failed: %s", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	// Window
+	SDL_Window* window = SDL_CreateWindow(
+		"ZeroG-Sample1",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		800, 800,
+		SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+	if (window == NULL) {
+		printf("SDL_CreateWindow() failed: %s\n", SDL_GetError());
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	// Run our main loop
+	bool running = true;
+	while (running) {
+
+		// Query SDL events, loop wrapped in a lambda so we can continue to next iteration of main
+		// loop. "return false;" == continue to next iteration
+		if (![&]() -> bool {
+			SDL_Event event = {};
+			while (SDL_PollEvent(&event) != 0) {
+				switch (event.type) {
+
+				case SDL_QUIT:
+					running = false;
+					return false;
+
+				case SDL_KEYUP:
+					//if (event.key.keysym.sym == SDLK_ESCAPE) {
+						running = false;
+						return false;
+					//}
+					break;
+				}
+			}
+			return true;
+		}()) continue;
+
+		// TODO: Rendering here
+	}
+
+	// Cleanup
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
