a56ac21a01e570be60b4b71a195bc40cfc8ba98b
Peter Hillerström <peter.hillerstrom93@gmail.com>
2019-02-21 19:27:44 +0100

    NaiveEcsEditor: Improved mask editor

--- engine/src/ph/ecs/naive/NaiveEcsEditor.cpp
+++ engine/src/ph/ecs/naive/NaiveEcsEditor.cpp
@@ -27,6 +27,8 @@
 #include <nfd.h>
 #endif
 
+#include <sfz/containers/HashMap.hpp>
+#include <sfz/strings/StringHashers.hpp>
 #include <sfz/Logging.hpp>
 #include <sfz/strings/StackString.hpp>
 #include <sfz/util/IO.hpp>
@@ -35,36 +37,353 @@
 
 namespace ph {
 
-using sfz::str32;
-
 // Static functions
 // ------------------------------------------------------------------------------------------------
 
-// Edits a component mask using a checkbox for each bit, returns whether mask was modified or not
-static bool componentMaskEditor(const char* identifier, ComponentMask& mask) noexcept
+static const char* byteToBinaryStringLookupTable[256] = {
+	"00000000",
+	"00000001",
+	"00000010",
+	"00000011",
+	"00000100",
+	"00000101",
+	"00000110",
+	"00000111",
+	"00001000",
+	"00001001",
+	"00001010",
+	"00001011",
+	"00001100",
+	"00001101",
+	"00001110",
+	"00001111",
+	"00010000",
+	"00010001",
+	"00010010",
+	"00010011",
+	"00010100",
+	"00010101",
+	"00010110",
+	"00010111",
+	"00011000",
+	"00011001",
+	"00011010",
+	"00011011",
+	"00011100",
+	"00011101",
+	"00011110",
+	"00011111",
+	"00100000",
+	"00100001",
+	"00100010",
+	"00100011",
+	"00100100",
+	"00100101",
+	"00100110",
+	"00100111",
+	"00101000",
+	"00101001",
+	"00101010",
+	"00101011",
+	"00101100",
+	"00101101",
+	"00101110",
+	"00101111",
+	"00110000",
+	"00110001",
+	"00110010",
+	"00110011",
+	"00110100",
+	"00110101",
+	"00110110",
+	"00110111",
+	"00111000",
+	"00111001",
+	"00111010",
+	"00111011",
+	"00111100",
+	"00111101",
+	"00111110",
+	"00111111",
+	"01000000",
+	"01000001",
+	"01000010",
+	"01000011",
+	"01000100",
+	"01000101",
+	"01000110",
+	"01000111",
+	"01001000",
+	"01001001",
+	"01001010",
+	"01001011",
+	"01001100",
+	"01001101",
+	"01001110",
+	"01001111",
+	"01010000",
+	"01010001",
+	"01010010",
+	"01010011",
+	"01010100",
+	"01010101",
+	"01010110",
+	"01010111",
+	"01011000",
+	"01011001",
+	"01011010",
+	"01011011",
+	"01011100",
+	"01011101",
+	"01011110",
+	"01011111",
+	"01100000",
+	"01100001",
+	"01100010",
+	"01100011",
+	"01100100",
+	"01100101",
+	"01100110",
+	"01100111",
+	"01101000",
+	"01101001",
+	"01101010",
+	"01101011",
+	"01101100",
+	"01101101",
+	"01101110",
+	"01101111",
+	"01110000",
+	"01110001",
+	"01110010",
+	"01110011",
+	"01110100",
+	"01110101",
+	"01110110",
+	"01110111",
+	"01111000",
+	"01111001",
+	"01111010",
+	"01111011",
+	"01111100",
+	"01111101",
+	"01111110",
+	"01111111",
+	"10000000",
+	"10000001",
+	"10000010",
+	"10000011",
+	"10000100",
+	"10000101",
+	"10000110",
+	"10000111",
+	"10001000",
+	"10001001",
+	"10001010",
+	"10001011",
+	"10001100",
+	"10001101",
+	"10001110",
+	"10001111",
+	"10010000",
+	"10010001",
+	"10010010",
+	"10010011",
+	"10010100",
+	"10010101",
+	"10010110",
+	"10010111",
+	"10011000",
+	"10011001",
+	"10011010",
+	"10011011",
+	"10011100",
+	"10011101",
+	"10011110",
+	"10011111",
+	"10100000",
+	"10100001",
+	"10100010",
+	"10100011",
+	"10100100",
+	"10100101",
+	"10100110",
+	"10100111",
+	"10101000",
+	"10101001",
+	"10101010",
+	"10101011",
+	"10101100",
+	"10101101",
+	"10101110",
+	"10101111",
+	"10110000",
+	"10110001",
+	"10110010",
+	"10110011",
+	"10110100",
+	"10110101",
+	"10110110",
+	"10110111",
+	"10111000",
+	"10111001",
+	"10111010",
+	"10111011",
+	"10111100",
+	"10111101",
+	"10111110",
+	"10111111",
+	"11000000",
+	"11000001",
+	"11000010",
+	"11000011",
+	"11000100",
+	"11000101",
+	"11000110",
+	"11000111",
+	"11001000",
+	"11001001",
+	"11001010",
+	"11001011",
+	"11001100",
+	"11001101",
+	"11001110",
+	"11001111",
+	"11010000",
+	"11010001",
+	"11010010",
+	"11010011",
+	"11010100",
+	"11010101",
+	"11010110",
+	"11010111",
+	"11011000",
+	"11011001",
+	"11011010",
+	"11011011",
+	"11011100",
+	"11011101",
+	"11011110",
+	"11011111",
+	"11100000",
+	"11100001",
+	"11100010",
+	"11100011",
+	"11100100",
+	"11100101",
+	"11100110",
+	"11100111",
+	"11101000",
+	"11101001",
+	"11101010",
+	"11101011",
+	"11101100",
+	"11101101",
+	"11101110",
+	"11101111",
+	"11110000",
+	"11110001",
+	"11110010",
+	"11110011",
+	"11110100",
+	"11110101",
+	"11110110",
+	"11110111",
+	"11111000",
+	"11111001",
+	"11111010",
+	"11111011",
+	"11111100",
+	"11111101",
+	"11111110",
+	"11111111",
+};
+
+static sfz::HashMap<str32, uint8_t> binaryStringToByteLookupMap;
+
+static const char* byteToBinaryString(uint8_t byte) noexcept
 {
-	const int32_t NUM_COLS = 16;
-	const int32_t NUM_ROWS = 4;
-	static_assert(NUM_COLS * NUM_ROWS == 64, "Think again");
+	return byteToBinaryStringLookupTable[byte];
+}
+
+static uint8_t binaryStringToByte(const char* binaryStr) noexcept
+{
+	const uint8_t* bytePtr = binaryStringToByteLookupMap.get(binaryStr);
+	if (bytePtr == nullptr) return 0;
+	return *bytePtr;
+}
+
+static void initializeComponentMaskEditor(str32 buffers[8], ComponentMask initialMask) noexcept
+{
+	for (uint64_t i = 0; i < 8; i++) {
+		int8_t byte = uint8_t((initialMask.rawMask >> (i * 8)) & uint64_t(0xFF));
+		const char* byteBinaryStr = byteToBinaryString(byte);
+		buffers[i].printf("%s", byteBinaryStr);
+	}
+}
+
+static int imguiOnlyBinaryFilter(ImGuiInputTextCallbackData* data) noexcept
+{
+	bool isBinaryChar = data->EventChar == '0' || data->EventChar == '1';
+	return isBinaryChar ? 0 : 1;
+}
+
+static bool componentMaskEditor(
+	const char* identifier,
+	str32 buffers[8],
+	ComponentMask& mask) noexcept
+{
+	const int32_t NUM_BITS_PER_FIELD = 8;
+	const int32_t NUM_FIELDS = 4;
+	const int32_t NUM_ROWS = 2;
+	static_assert(NUM_BITS_PER_FIELD * NUM_FIELDS * NUM_ROWS == 64, "Think again");
 
 	bool bitsModified = false;
 
-	for (int32_t offs = 0; offs < NUM_ROWS; offs++) {
-		for (int32_t i = NUM_COLS - 1; i >= 0; i--) {
+	for (int32_t rowIdx = 0; rowIdx < NUM_ROWS; rowIdx++) {
+		for (int32_t fieldIdx = NUM_FIELDS - 1; fieldIdx >= 0; fieldIdx--) {
 
-			uint32_t bitIndex = uint32_t(i + offs * NUM_COLS);
+			int32_t byteIdx = rowIdx * NUM_FIELDS + fieldIdx;
+			uint64_t shiftOffset = 8 * byteIdx;
 
-			ImGui::BeginGroup();
-			ImGui::Text("%s", str32("%02u", bitIndex).str);
+			ImGuiInputTextFlags inputFlags = 0;
+			inputFlags |= ImGuiInputTextFlags_EnterReturnsTrue;
+			inputFlags |= ImGuiInputTextFlags_CallbackCharFilter;
 
-			bool b = mask.hasComponentType(bitIndex);
-			if (ImGui::Checkbox(str32("##%s_%u", identifier, bitIndex), &b)) {
+			ImGui::PushItemWidth(85.0f);
+			bool modified = ImGui::InputText(
+				str32("##%s_%u", identifier, byteIdx),
+				buffers[byteIdx],
+				9, // We only allow 8 characters (bits) per byte
+				inputFlags,
+				imguiOnlyBinaryFilter);
+			ImGui::PopItemWidth();
+
+			if (modified) {
+				const uint8_t modifiedByte = binaryStringToByte(buffers[byteIdx]);
+				const uint64_t keepMask = ~(uint64_t(0xFF) << shiftOffset);
+				const uint64_t insertByte = uint64_t(modifiedByte) << shiftOffset;
+				const uint64_t modifiedMask = (mask.rawMask & keepMask) | insertByte;
+				mask.rawMask = modifiedMask;
 				bitsModified = true;
-				mask.setComponentType(bitIndex, b);
 			}
 
-			ImGui::EndGroup();
-			if (i != 0) ImGui::SameLine();
+			ImGui::SameLine();
+		}
+
+		if(rowIdx == 0) {
+			uint32_t byte0 = uint32_t(mask.rawMask & 0xFF);
+			uint32_t byte1 = uint32_t((mask.rawMask >> 8) & 0xFF);
+			uint32_t byte2 = uint32_t((mask.rawMask >> 16) & 0xFF);
+			uint32_t byte3 = uint32_t((mask.rawMask >> 24) & 0xFF);
+			ImGui::Text("[%02X %02X %02X %02X]", byte3, byte2, byte1, byte0);
+		}
+		else {
+			uint32_t byte4 = uint32_t((mask.rawMask >> 32) & 0xFF);
+			uint32_t byte5 = uint32_t((mask.rawMask >> 40) & 0xFF);
+			uint32_t byte6 = uint32_t((mask.rawMask >> 48) & 0xFF);
+			uint32_t byte7 = uint32_t((mask.rawMask >> 56) & 0xFF);
+			ImGui::Text("[%02X %02X %02X %02X]", byte7, byte6, byte5, byte4);
 		}
 	}
 
@@ -74,9 +393,25 @@ static bool componentMaskEditor(const char* identifier, ComponentMask& mask) noe
 // NaiveEcsEditor: State methods
 // ------------------------------------------------------------------------------------------------
 
-void NaiveEcsEditor::init(ComponentInfo* componentInfos, uint32_t numComponentInfos)
+void NaiveEcsEditor::init(
+	const char* windowName,
+	ComponentInfo* componentInfos,
+	uint32_t numComponentInfos,
+	sfz::Allocator* allocator)
 {
 	this->destroy();
+
+	// Initialize binary string to byte lookup map
+	if (binaryStringToByteLookupMap.size() == 0) {
+		binaryStringToByteLookupMap.create(512, allocator);
+		for (uint32_t i = 0; i < 256; i++) {
+			binaryStringToByteLookupMap[byteToBinaryStringLookupTable[i]] = uint8_t(i);
+		}
+	}
+
+	// Initialize some state
+	mWindowName.printf("%s", windowName);
+	initializeComponentMaskEditor(mFilterMaskEditBuffers, mFilterMask);
 
 	// Set active bit component info
 	mComponentInfos[0].componentName.printf("00 - Active bit");
@@ -112,20 +447,30 @@ void NaiveEcsEditor::init(ComponentInfo* componentInfos, uint32_t numComponentIn
 
 void NaiveEcsEditor::swap(NaiveEcsEditor& other) noexcept
 {
+	std::swap(this->mWindowName, other.mWindowName);
 	for (uint32_t i = 0; i < 64; i++) {
 		std::swap(this->mComponentInfos[i], other.mComponentInfos[i]);
 	}
 	std::swap(this->mFilterMask, other.mFilterMask);
+	for (uint32_t i = 0; i < 8; i++) {
+		std::swap(this->mFilterMaskEditBuffers[i], other.mFilterMaskEditBuffers[i]);
+		std::swap(this->mEntityMaskEditBuffers[i], other.mEntityMaskEditBuffers[i]);
+	}
 	std::swap(this->mCurrentSelectedEntity, other.mCurrentSelectedEntity);
 }
 
 void NaiveEcsEditor::destroy() noexcept
 {
+	mWindowName.printf("");
 	for (uint32_t i = 0; i < 64; i++) {
 		this->mComponentInfos[i] = ReducedComponentInfo();
 	}
 	mFilterMask = ComponentMask::activeMask();
-	mCurrentSelectedEntity = 0;
+	for (uint32_t i = 0; i < 8; i++) {
+		mFilterMaskEditBuffers[i].printf("");
+		mEntityMaskEditBuffers[i].printf("");
+	}
+	mCurrentSelectedEntity = ~0u;
 }
 
 // NaiveEcsEditor: Methods
@@ -134,12 +479,12 @@ void NaiveEcsEditor::destroy() noexcept
 void NaiveEcsEditor::render(NaiveEcsHeader* ecs) noexcept
 {
 	// Begin window
-	ImGui::SetNextWindowContentSize(sfz::vec2(550.0f, 500.0f));
+	ImGui::SetNextWindowContentSize(sfz::vec2(600.0f, 750.0f));
 	ImGuiWindowFlags windowFlags = 0;
 	//windowFlags |= ImGuiWindowFlags_NoResize;
-	windowFlags |= ImGuiWindowFlags_NoScrollbar;
+	//windowFlags |= ImGuiWindowFlags_NoScrollbar;
 	windowFlags |= ImGuiWindowFlags_NoFocusOnAppearing;
-	ImGui::Begin("Naive ECS Editor", nullptr, windowFlags);
+	ImGui::Begin(mWindowName.str, nullptr, windowFlags);
 
 	// End window and return if no ECS system
 	if (ecs == nullptr) {
@@ -233,9 +578,7 @@ void NaiveEcsEditor::render(NaiveEcsHeader* ecs) noexcept
 	ImGui::Spacing();
 
 	// Currently selected entities component mask
-	if (ImGui::CollapsingHeader("Component mask filter")) {
-		componentMaskEditor("FilterMaskBit", mFilterMask);
-	}
+	componentMaskEditor("FilterMaskBit", mFilterMaskEditBuffers, mFilterMask);
 
 	// Spacing and separator between the different type of views
 	ImGui::Spacing();
@@ -260,7 +603,10 @@ void NaiveEcsEditor::render(NaiveEcsHeader* ecs) noexcept
 			str32 entityStr("%08u", entity);
 			bool selected = mCurrentSelectedEntity == entity;
 			bool activated = ImGui::Selectable(entityStr, selected);
-			if (activated) mCurrentSelectedEntity = entity;
+			if (activated) {
+				mCurrentSelectedEntity = entity;
+				initializeComponentMaskEditor(mEntityMaskEditBuffers, masks[entity]);
+			}
 
 			// Non-active entites are greyed out
 			if (!active) ImGui::PopStyleColor();
@@ -292,15 +638,12 @@ void NaiveEcsEditor::render(NaiveEcsHeader* ecs) noexcept
 	ImGui::BeginGroup();
 
 	// Only show entity edit menu if an active entity is selected
-	if (masks[mCurrentSelectedEntity].active()) {
+	bool selectedEntityIsActive =
+		mCurrentSelectedEntity < ecs->maxNumEntities && masks[mCurrentSelectedEntity].active();
+	if (selectedEntityIsActive) {
 
 		// Currently selected entities component mask
-		if (ImGui::CollapsingHeader("Component Mask")) {
-			ComponentMask mask = masks[mCurrentSelectedEntity];
-			if (componentMaskEditor("EntityMask", mask)) {
-				masks[mCurrentSelectedEntity] = mask;
-			}
-		}
+		componentMaskEditor("MaskEditor", mEntityMaskEditBuffers, masks[mCurrentSelectedEntity]);
 
 		ImGui::Spacing();
 		ImGui::Separator();
