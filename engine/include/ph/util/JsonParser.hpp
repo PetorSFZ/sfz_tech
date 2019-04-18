06304743d1e53c003d12f76f9e672e3e6b05c7fb
Peter Hillerström <peter.hillerstrom93@gmail.com>
2019-04-18 21:59:10 +0200

    Added json parser wrapper around sajson

---
+++ engine/include/ph/util/JsonParser.hpp
@@ -0,0 +1,158 @@
+// Copyright (c) Peter Hillerström (skipifzero.com, peter@hstroem.se)
+//               For other contributors see Contributors.txt
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+#pragma once
+
+#include <cstdint>
+
+#include <sfz/memory/Allocator.hpp>
+
+namespace ph {
+
+// ParsedJsonNodeType enum
+// ------------------------------------------------------------------------------------------------
+
+// The different types of nodes
+enum class ParsedJsonNodeType : uint32_t {
+	// Undefined node, not valid to do any operations on
+	NONE = 0,
+
+	// Non-leaf nodes (does not contain values, but other nodes)
+	MAP,
+	ARRAY,
+
+	// Leaf nodes (value can be accessed directly from node)
+	BOOL,
+	INTEGER,
+	FLOATING_POINT,
+	STRING,
+};
+
+// ParsedJsonNode class
+// ------------------------------------------------------------------------------------------------
+
+// Size of the implementation of ParsedJsonNode in bytes
+constexpr uint32_t PARSED_JSON_NODE_IMPL_SIZE = 32;
+
+// Represents a node in a ParsedJson instance
+//
+// Used to traverse and access contents of a ParsedJson. NONE nodes are considered invalid and are
+// used as error codes for invalid accesses. Default constructed ParsedJsonNodes are also NONE.
+class ParsedJsonNode final {
+public:
+	// Constructors & destructors
+	// --------------------------------------------------------------------------------------------
+
+	ParsedJsonNode() noexcept = default;
+	ParsedJsonNode(const ParsedJsonNode&) = delete;
+	ParsedJsonNode& operator= (const ParsedJsonNode&) = delete;
+	ParsedJsonNode(ParsedJsonNode&& other) noexcept { this->swap(other); }
+	ParsedJsonNode& operator= (ParsedJsonNode&& other) noexcept { this->swap(other); return *this; }
+	~ParsedJsonNode() noexcept { this->destroy(); }
+
+	static ParsedJsonNode createFromImplDefined(const void* implDefined) noexcept;
+
+	// State methods
+	// --------------------------------------------------------------------------------------------
+
+	void swap(ParsedJsonNode& other) noexcept;
+	void destroy() noexcept;
+
+	// Methods (all nodes)
+	// --------------------------------------------------------------------------------------------
+
+	// Returns type of node. NONE if the node is invalid for some reason.
+	ParsedJsonNodeType type() const noexcept;
+
+	// Returns whether the node is valid or not. NONE nodes are considered invalid.
+	bool isValid() const noexcept { return mActive; }
+
+	// Methods (non-leaf nodes)
+	// --------------------------------------------------------------------------------------------
+
+	// Accesses a node in a map, returns NONE on invalid access or if not map
+	ParsedJsonNode accessMap(const char* nodeName) const noexcept;
+
+	// Length of the array, returns 0 if not an array node
+	uint32_t arrayLength() const noexcept;
+
+	// Accesses a node in the array, returns NONE on invalid access or if not array
+	ParsedJsonNode accessArray(uint32_t index) const noexcept;
+
+	// Methods (leaf nodes)
+	// --------------------------------------------------------------------------------------------
+
+	bool value(bool& valueOut) const noexcept;
+	bool value(int32_t& valueOut) const noexcept;
+	bool value(float& valueOut) const noexcept;
+	bool value(double& valueOut) const noexcept;
+	bool value(char* strOut, uint32_t strCapacity) const noexcept;
+	uint32_t stringLength() const noexcept; // Returns 0 if not a string
+
+private:
+	// Private members
+	// --------------------------------------------------------------------------------------------
+
+	uint8_t mImpl[PARSED_JSON_NODE_IMPL_SIZE] = {};
+	bool mActive = false;
+};
+
+// ParsedJson class
+// ------------------------------------------------------------------------------------------------
+
+struct ParsedJsonImpl; // Pimpl pattern
+
+// A class that represents a parsed JSON file
+//
+// Parse a JSON file using either ParsedJson::parseString() or ParsedJson::parseFile(). The parsed
+// contents can then be accessed by recursively accessing the nodes, starting with the root node.
+class ParsedJson final {
+public:
+	// Constructors & destructors
+	// ---------------------------------------------------------------------------------------------
+
+	ParsedJson() noexcept = default;
+	ParsedJson(const ParsedJson&) = delete;
+	ParsedJson& operator= (const ParsedJson&) = delete;
+	ParsedJson(ParsedJson&& other) noexcept { this->swap(other); }
+	ParsedJson& operator= (ParsedJson&& other) noexcept { this->swap(other); return *this; }
+	~ParsedJson() noexcept { this->destroy(); }
+
+	static ParsedJson parseString(const char* jsonString, sfz::Allocator* allocator) noexcept;
+	static ParsedJson parseFile(const char* jsonPath, sfz::Allocator* allocator) noexcept;
+
+	// State methods
+	// ---------------------------------------------------------------------------------------------
+
+	void swap(ParsedJson& other) noexcept;
+	void destroy() noexcept;
+
+	// Methods
+	// ---------------------------------------------------------------------------------------------
+
+	ParsedJsonNode root() const noexcept;
+
+private:
+	// Private members
+	// ---------------------------------------------------------------------------------------------
+
+	ParsedJsonImpl* mImpl = nullptr;
+};
+
+} // namespace ph
---
+++ engine/src/ph/util/JsonParser.cpp
@@ -0,0 +1,363 @@
+// Copyright (c) Peter Hillerström (skipifzero.com, peter@hstroem.se)
+//               For other contributors see Contributors.txt
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+#include "ph/util/JsonParser.hpp"
+
+#include <algorithm>
+#include <cstring>
+
+#include <sfz/PushWarnings.hpp>
+#define SAJSON_NO_STD_STRING
+#include "sajson.h"
+#include <sfz/PopWarnings.hpp>
+
+#include <sfz/Logging.hpp>
+#include <sfz/memory/New.hpp>
+#include <sfz/util/IO.hpp>
+
+namespace ph {
+
+// Statics
+// ------------------------------------------------------------------------------------------------
+
+static_assert(PARSED_JSON_NODE_IMPL_SIZE >= sizeof(sajson::value), "ParsedJsonNode is too small");
+
+static const sajson::value& castToSajsonValue(const uint8_t* memory) noexcept
+{
+	return *reinterpret_cast<const sajson::value*>(memory);
+}
+
+// ParsedJsonNode: Constructors & destructors
+// ------------------------------------------------------------------------------------------------
+
+ParsedJsonNode ParsedJsonNode::createFromImplDefined(const void* implDefined) noexcept
+{
+	const sajson::value& value = *reinterpret_cast<const sajson::value*>(implDefined);
+	ParsedJsonNode node;
+	new (node.mImpl) sajson::value(value);
+	node.mActive = true;
+	return node;
+}
+
+// ParsedJsonNode: State methods
+// ------------------------------------------------------------------------------------------------
+
+void ParsedJsonNode::swap(ParsedJsonNode& other) noexcept
+{
+	std::swap(this->mImpl, other.mImpl);
+	std::swap(this->mActive, other.mActive);
+}
+
+void ParsedJsonNode::destroy() noexcept
+{
+	if (!mActive) return;
+	reinterpret_cast<sajson::value*>(mImpl)->~value();
+	memset(mImpl, 0, sizeof(mImpl));
+	mActive = false;
+}
+
+// ParsedJsonNode: Methods (all nodes)
+// ------------------------------------------------------------------------------------------------
+
+ParsedJsonNodeType ParsedJsonNode::type() const noexcept
+{
+	// Return NONE type if not active
+	if (!mActive) return ParsedJsonNodeType::NONE;
+
+	const sajson::value& value = castToSajsonValue(mImpl);
+	sajson::type type = value.get_type();
+	switch (type) {
+	case sajson::TYPE_INTEGER: return ParsedJsonNodeType::INTEGER;
+	case sajson::TYPE_DOUBLE: return ParsedJsonNodeType::FLOATING_POINT;
+	case sajson::TYPE_NULL: return ParsedJsonNodeType::NONE;
+	case sajson::TYPE_FALSE: return ParsedJsonNodeType::BOOL;
+	case sajson::TYPE_TRUE: return ParsedJsonNodeType::BOOL;
+	case sajson::TYPE_STRING: return ParsedJsonNodeType::STRING;
+	case sajson::TYPE_ARRAY: return ParsedJsonNodeType::ARRAY;
+	case sajson::TYPE_OBJECT: return ParsedJsonNodeType::MAP;
+	}
+	sfz_assert_debug(false);
+	return ParsedJsonNodeType::MAP;
+}
+
+// ParsedJsonNode: Methods (non-leaf nodes)
+// ------------------------------------------------------------------------------------------------
+
+ParsedJsonNode ParsedJsonNode::accessMap(const char* nodeName) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return NONE if wrong type
+	if (this->type() != ParsedJsonNodeType::MAP) return ParsedJsonNode();
+
+	// Get value in map
+	sajson::string nodeNameStr(nodeName, std::strlen(nodeName));
+	sajson::value elementValue = value.get_value_of_key(nodeNameStr);
+
+	// Return NONE node if invalid access
+	if (elementValue.get_type() == sajson::TYPE_NULL) return ParsedJsonNode();
+
+	// Return node
+	return ParsedJsonNode::createFromImplDefined(&elementValue);
+}
+
+uint32_t ParsedJsonNode::arrayLength() const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return 0 if node is of wrong type
+	if (this->type() != ParsedJsonNodeType::INTEGER) return 0;
+
+	return (uint32_t)value.get_length();
+}
+
+ParsedJsonNode ParsedJsonNode::accessArray(uint32_t index) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return NONE if wrong type
+	if (this->type() != ParsedJsonNodeType::ARRAY) return ParsedJsonNode();
+
+	// Return NONE if out of range
+	if (index >= (uint32_t)value.get_length()) return ParsedJsonNode();
+
+	// Return array element
+	sajson::value elementValue = value.get_array_element(index);
+	return ParsedJsonNode::createFromImplDefined(&elementValue);
+}
+
+// ParsedJsonNode: Methods (leaf nodes)
+// ------------------------------------------------------------------------------------------------
+
+bool ParsedJsonNode::value(bool& valueOut) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	if (this->type() != ParsedJsonNodeType::BOOL) return false;
+
+	// Return value
+	valueOut = value.get_type() == sajson::TYPE_TRUE;
+	return true;
+}
+
+bool ParsedJsonNode::value(int32_t& valueOut) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	if (this->type() != ParsedJsonNodeType::INTEGER) return false;
+
+	// Return value
+	valueOut = value.get_integer_value();
+	return true;
+}
+
+bool ParsedJsonNode::value(float& valueOut) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	ParsedJsonNodeType t = this->type();
+	if (t != ParsedJsonNodeType::FLOATING_POINT && t != ParsedJsonNodeType::INTEGER) return false;
+
+	// Return value
+	if (t == ParsedJsonNodeType::FLOATING_POINT) {
+		valueOut = (float)value.get_double_value();
+	}
+	else {
+		valueOut = (float)value.get_integer_value();
+	}
+	return true;
+}
+
+bool ParsedJsonNode::value(double& valueOut) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	ParsedJsonNodeType t = this->type();
+	if (t != ParsedJsonNodeType::FLOATING_POINT && t != ParsedJsonNodeType::INTEGER) return false;
+
+	// Return value
+	if (t == ParsedJsonNodeType::FLOATING_POINT) {
+		valueOut = value.get_double_value();
+	}
+	else {
+		valueOut = (double)value.get_integer_value();
+	}
+	return true;
+}
+
+bool ParsedJsonNode::value(char* strOut, uint32_t strCapacity) const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	if (this->type() != ParsedJsonNodeType::STRING) return false;
+
+	// Check that capacity is enough
+	uint32_t strLen = (uint32_t)value.get_string_length();
+	if (strLen >= strCapacity) return false; // >= so we guarantee space for null-terminator
+
+	// Copy out string
+	std::memcpy(strOut, value.as_cstring(), strLen);
+	strOut[strLen] = '\0'; // Ensure string is null-terminated
+	return true;
+}
+
+uint32_t ParsedJsonNode::stringLength() const noexcept
+{
+	sfz_assert_debug(this->mActive);
+	const sajson::value& value = castToSajsonValue(mImpl);
+
+	// Return false if node is of wrong type
+	if (this->type() != ParsedJsonNodeType::STRING) return 0;
+
+	// Return string length
+	uint32_t strLen = (uint32_t)value.get_string_length();
+	return strLen;
+}
+
+// ParsedJson: Implementation
+// ------------------------------------------------------------------------------------------------
+
+struct ParsedJsonImpl {
+	sfz::Allocator* allocator = nullptr;
+
+	char* jsonStringCopy = nullptr;
+	sajson::mutable_string_view jsongStringView;
+
+	size_t* astMemory = nullptr;
+	sajson::bounded_allocation astAllocation = sajson::bounded_allocation(nullptr, 0);
+
+	sajson::document* doc = nullptr;
+};
+
+// ParsedJson: Constructors & destructors
+// ------------------------------------------------------------------------------------------------
+
+ParsedJson ParsedJson::parseString(const char* jsonString, sfz::Allocator* allocator) noexcept
+{
+	// Ensure json string is not nullptr
+	if (jsonString == nullptr) {
+		SFZ_ERROR("JSON", "JSON string may not be nullptr");
+		return ParsedJson();
+	}
+
+	// Allocate implementation
+	ParsedJson parsedJson;
+	parsedJson.mImpl = sfz::sfzNew<ParsedJsonImpl>(allocator);
+	ParsedJsonImpl& impl = *parsedJson.mImpl;
+	impl.allocator = allocator;
+
+	// Calculate length of string and copy it
+	uint64_t jsonStringLen = std::strlen(jsonString);
+	if (jsonStringLen == 0) {
+		sfz::sfzDelete(parsedJson.mImpl, allocator);
+		SFZ_ERROR("JSON", "JSON string must be longer than 0");
+		return ParsedJson();
+	}
+	impl.jsonStringCopy = reinterpret_cast<char*>(
+		allocator->allocate(jsonStringLen + 1, 32, "json string copy"));
+	std::memcpy(impl.jsonStringCopy, jsonString, jsonStringLen);
+	impl.jsongStringView = sajson::mutable_string_view(jsonStringLen, impl.jsonStringCopy);
+
+	// Allocate memory for ast
+	// Assume a constant factor times the json string itself is enough memory for the AST
+	const uint64_t SIZE_FACTOR = 4;
+	uint64_t allocationSize = SIZE_FACTOR * jsonStringLen;
+	impl.astMemory =
+		reinterpret_cast<size_t*>(allocator->allocate(allocationSize, 32, "sajson ast"));
+	impl.astAllocation = sajson::bounded_allocation(impl.astMemory, allocationSize);
+
+	// Parse json string
+	impl.doc = sfz::sfzNew<sajson::document>(allocator, sajson::parse(impl.astAllocation, impl.jsongStringView));
+	if (!impl.doc->is_valid()) {
+		parsedJson.destroy();
+		SFZ_ERROR("JSON", "Json parse failed at line %i: %s",
+			(int)impl.doc->get_error_line(), impl.doc->get_error_message_as_cstring());
+		return ParsedJson();
+	}
+
+	return parsedJson;
+}
+
+ParsedJson ParsedJson::parseFile(const char* jsonPath, sfz::Allocator* allocator) noexcept
+{
+	sfz::DynString jsonString = sfz::readTextFile(jsonPath, allocator);
+	if (jsonString.size() == 0) {
+		SFZ_ERROR("JSON", "Failed to load JSON file at: %s", jsonPath);
+		return ParsedJson();
+	}
+	return ParsedJson::parseString(jsonString.str(), allocator);
+}
+
+// ParsedJson: State methods
+// ------------------------------------------------------------------------------------------------
+
+void ParsedJson::swap(ParsedJson& other) noexcept
+{
+	std::swap(this->mImpl, other.mImpl);
+}
+
+void ParsedJson::destroy() noexcept
+{
+	if (mImpl == nullptr) return;
+	sfz::Allocator* allocator = mImpl->allocator;
+
+	// Deallocate sajson document
+	if (mImpl->doc != nullptr) {
+		sfz::sfzDelete(mImpl->doc, allocator);
+	}
+
+	// Deallocate json string copy
+	if (mImpl->jsonStringCopy != nullptr) {
+		allocator->deallocate(mImpl->jsonStringCopy);
+	}
+
+	// Deallocate sajson ast
+	if (mImpl->astMemory != nullptr) {
+		allocator->deallocate(mImpl->astMemory);
+	}
+
+	// Deallocate implementation
+	sfz::sfzDelete(mImpl, allocator);
+	mImpl = nullptr;
+}
+
+// ParsedJson: State methods
+// ------------------------------------------------------------------------------------------------
+
+ParsedJsonNode ParsedJson::root() const noexcept
+{
+	sfz_assert_debug(mImpl->doc->is_valid());
+	sajson::value value = mImpl->doc->get_root();
+	return ParsedJsonNode::createFromImplDefined(&value);
+}
+
+} // namespace ph
